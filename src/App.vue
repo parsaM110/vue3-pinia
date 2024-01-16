934350fdd41f95a8e8fd0516eab8a5e147c1ac70
parsaM110 <pmomeni80@gmail.com>
2024-01-16 20:37:55 +0330

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
