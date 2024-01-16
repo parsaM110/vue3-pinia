9eb71fdddc69fe9b7728607e1c23af5d83ebe0cd
parsaM110 <pmomeni80@gmail.com>
2024-01-16 23:50:49 +0330

    some small modification in titles

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,13 @@
       <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo" />
       <h1>{{ taskStore.name }}</h1>
     </header>
+    <!-- task list -->
     <div class="task-list">
+      <p>all tasks</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
-      <h3>favorites</h3>
+      <p>favorites</p>
       <div v-for="task in taskStore.favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
